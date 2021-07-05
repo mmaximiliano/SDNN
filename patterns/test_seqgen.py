85a95e792615632d2b183efc6006090665125a2e
Maxi Martino <Maxii.martino@gmail.com>
2021-07-05 16:52:03 -0300

    Longer sequence (34.000)

--- patterns/test_seqgen.py
+++ patterns/test_seqgen.py
@@ -21,8 +21,8 @@ pan_functions.seed_torch(seed=0)
 # parameters of the dataset - this are only have the purpose of testing - use defaults
 nframes=5 # number of frames to divide the event stream
 patt=[1,2,3] # pattern to copy throught the sequence
-qsmp=50 # number of samples of each digit to include in the sequence
-qrep=50 # number of times the patter is repeated on the sequence
+qsmp=500 # number of samples of each digit to include in the sequence
+qrep=600 # number of times the patter is repeated on the sequence
 
 train_seq = pan_functions.seqPatternNMNIST(root=root_dataset,nframes=nframes, 
                       patt=patt, qsmp=qsmp, qrep=qrep)
