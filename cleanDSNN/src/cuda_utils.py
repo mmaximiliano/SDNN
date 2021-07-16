69fcb17da21fa58859e9cefac8c0a48fc29820f0
Maxi Martino <Maxii.martino@gmail.com>
2021-07-16 16:19:08 -0300

    fix model

--- cleanDSNN/src/cuda_utils.py
+++ cleanDSNN/src/cuda_utils.py
@@ -22,21 +22,20 @@ def conv_step(S, I, V, C, s, w, stride, th, alpha, beta, delay):
     if V[idx, idy, idz] > th:
         if C[idx, idy, idz] == 0:
             V[idx, idy, idz] = 0.
+            I[idx, idy, idz] = (I[idx, idy, idz] * beta)
 
     # Calculate the membrance potential
-    U = 0.
     In = 0.
     for k in range(w.shape[2]):
         for j in range(w.shape[1]):
             for i in range(w.shape[0]):
-                U = (U*alpha) + In
-                In = (In*beta) + (w[i, j, k, idz] * s[idx*stride + i, idy*stride+j, k])
+                In += (w[i, j, k, idz] * s[idx*stride + i, idy*stride+j, k])
 
     # Calculate potential for this timestep
     V_prev = V[idx, idy, idz]
     I_prev = I[idx, idy, idz]
-    V[idx, idy, idz] += U
-    I[idx, idy, idz] += In
+    V[idx, idy, idz] = (V[idx, idy, idz] * alpha) + I[idx, idy, idz]
+    I[idx, idy, idz] = (I[idx, idy, idz] * beta) + In
 
     # Set the counter if neuron reaches threshold
     if C[idx, idy, idz] == 0:
@@ -45,7 +44,7 @@ def conv_step(S, I, V, C, s, w, stride, th, alpha, beta, delay):
     else:
         V[idx, idy, idz] = V_prev
         I[idx, idy, idz] = I_prev
-        C[idx, idy, idz] = C[idx, idy, idz] - 1
+        C[idx, idy, idz] -= 1
 
     if V[idx, idy, idz] > th:
         if C[idx, idy, idz] == 0:
