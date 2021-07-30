fb94c4156db7aa1240807aeb52144fbb26bd0565
Maxi Martino <Maxii.martino@gmail.com>
2021-07-30 00:08:36 -0300

    Update main.py

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -50,7 +50,7 @@ def main():
                        'alpha': 1., 'beta': 0., 'delay': 0}]
     weight_params = {'mean': 0.8, 'std': 0.01}
 
-    max_learn_iter = [0, 14000, 0, 100, 0, 100, 0]
+    max_learn_iter = [0, 14000, 0, 10000, 0, 100, 0]
     stdp_params = {'max_learn_iter': max_learn_iter,
                    'stdp_per_layer': [0, 10, 0, 4, 0, 2],
                    'max_iter': sum(max_learn_iter),
