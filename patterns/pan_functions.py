253135b0f8205e7ee924a3b3cad7d4d61ee42aa4
Maxi Martino <Maxii.martino@gmail.com>
2021-08-04 17:02:04 -0300

    Refactor + optional pattern + exclude digits

--- patterns/pan_functions.py
+++ patterns/pan_functions.py
@@ -13,6 +13,8 @@ import numpy as np
 from random import shuffle, randint
 import random
 import torch
+
+
 #############################################################################
 def seed_torch(seed=1029):
     random.seed(seed)
@@ -21,6 +23,7 @@ def seed_torch(seed=1029):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+
 
 def show_td(data, width, height, wait_delay=1):
     """Displays the TD events (change detection ATIS or DVS events)
@@ -33,15 +36,15 @@ def show_td(data, width, height, wait_delay=1):
     td_img = np.ones((height, width), dtype=np.uint8)
     while frame_start < t_max:
         frame_data = data[(data.ts >= frame_start) & (data.ts < frame_end)]
-        
+
         if frame_data.size > 0:
             td_img.fill(128)
 
-            #with timer.Timer() as em_playback_timer:
+            # with timer.Timer() as em_playback_timer:
             for datum in np.nditer(frame_data):
                 td_img[datum['y'].item(0), datum['x'].item(0)] = datum['p'].item(0)
-            #print 'prepare td frame by iterating events took %s seconds'
-            #%em_playback_timer.secs
+            # print 'prepare td frame by iterating events took %s seconds'
+            # %em_playback_timer.secs
 
             td_img = np.piecewise(td_img, [td_img == 0, td_img == 1, td_img == 128], [0, 255, 128])
             cv2.imshow('img', td_img)
@@ -53,6 +56,7 @@ def show_td(data, width, height, wait_delay=1):
     cv2.destroyAllWindows()
     return
 
+
 def read_dataset(filename):
     """Reads in the TD events contained in the N-MNIST/N-CALTECH101 dataset file specified by 'filename'"""
     f = open(filename, 'rb')
@@ -62,30 +66,32 @@ def read_dataset(filename):
 
     all_y = raw_data[1::5]
     all_x = raw_data[0::5]
-    all_p = (raw_data[2::5] & 128) >> 7 #bit 7
+    all_p = (raw_data[2::5] & 128) >> 7  # bit 7
     all_ts = ((raw_data[2::5] & 127) << 16) | (raw_data[3::5] << 8) | (raw_data[4::5])
 
-    #Process time stamp overflow events
+    # Process time stamp overflow events
     time_increment = 2 ** 13
     overflow_indices = np.where(all_y == 240)[0]
     for overflow_index in overflow_indices:
         all_ts[overflow_index:] += time_increment
 
-    #Everything else is a proper td spike
+    # Everything else is a proper td spike
     td_indices = np.where(all_y != 240)[0]
     num_events = td_indices.size
-    
-    data = np.rec.array(None, dtype=[('x', np.uint16), ('y', np.uint16), ('p', np.uint8), ('ts', np.uint64)], shape=(num_events))
+
+    data = np.rec.array(None, dtype=[('x', np.uint16), ('y', np.uint16), ('p', np.uint8), ('ts', np.uint64)],
+                        shape=(num_events))
     data.x = all_x[td_indices]
     width = data.x.max() + 1
     data.y = all_y[td_indices]
     height = data.y.max() + 1
     data.ts = all_ts[td_indices]
     data.p = all_p[td_indices]
-    return data, width, height  
+    return data, width, height
+
 
 class seqPatternNMNIST(Dataset):
-    def __init__(self, root, nframes=5, patt=[1,2,3], qsmp=3000, qrep=250):
+    def __init__(self, root, nframes=5, patt=[1, 2, 3], qsmp=3000, qrep=250):
         # generata a random list of all the files in nmnist with qsmp
         self.nframes = nframes
         samples_list_tmp = []
@@ -97,58 +103,60 @@ class seqPatternNMNIST(Dataset):
         nros.sort()
         for nro in nros:
             # get file list
-            files = os.listdir(os.path.join(root,nro))
-            assert(len(files) > qsmp)
+            files = os.listdir(os.path.join(root, nro))
+            assert (len(files) > qsmp)
             shuffle(files)
             for i in range(qsmp):
-                samples_list_tmp.append(os.path.join(root,nro,files[i]))
+                samples_list_tmp.append(os.path.join(root, nro, files[i]))
                 target_list_tmp.append(0)
                 digit_list_tmp.append(nro)
             if int(nro) in self.patt:
-                #print('%s in patt' % nro)
+                # print('%s in patt' % nro)
                 shuffle(files)
                 cumpatt = []
                 for i in range(qrep):
-                    cumpatt.append(os.path.join(root,nro,files[i]))
-                self.pattern_list.append((cumpatt,nro))
+                    cumpatt.append(os.path.join(root, nro, files[i]))
+                self.pattern_list.append((cumpatt, nro))
         # now I need to shuffle the lists together
-        c = list(zip(samples_list_tmp, target_list_tmp,digit_list_tmp))
+        c = list(zip(samples_list_tmp, target_list_tmp, digit_list_tmp))
         shuffle(c)
         samples_list_tmp, target_list_tmp, digit_list_tmp = zip(*c)
-        self.samples_list, self.target_list,self.digit_list = list(samples_list_tmp), list(target_list_tmp), list(digit_list_tmp)
-        #print(self.samples_list[0])
-        #print('length of pattern lists ', len(self.pattern_list))
+        self.samples_list, self.target_list, self.digit_list = list(samples_list_tmp), list(target_list_tmp), list(
+            digit_list_tmp)
+        # print(self.samples_list[0])
+        # print('length of pattern lists ', len(self.pattern_list))
         # now, insert qrep times the pattern in samples list
         nsmp_tmp = len(self.samples_list)
         gap = int(nsmp_tmp / (qrep))
-        #print('gap', gap)
+        # print('gap', gap)
         dx = int(0.1 * gap)
-        #print('dx', dx)
-        for k in range(1,qrep+1):
-            kind = k*gap + randint(-dx,dx)
-            #print('kind', kind)
+        # print('dx', dx)
+        for k in range(1, qrep + 1):
+            kind = k * gap + randint(-dx, dx)
+            # print('kind', kind)
             for i in range(len(self.pattern_list)):
-                #print(self.pattern_list[i][0])
-                self.samples_list.insert(kind+i,self.pattern_list[i][0][k-1])
-                self.target_list.insert(kind+i,1)
-                self.digit_list.insert(kind+i, self.pattern_list[i][1])
-                
+                # print(self.pattern_list[i][0])
+                self.samples_list.insert(kind + i, self.pattern_list[i][0][k - 1])
+                self.target_list.insert(kind + i, 1)
+                self.digit_list.insert(kind + i, self.pattern_list[i][1])
+
     def __len__(self):
         return len(self.samples_list)
-    
+
     def __getitem__(self, index):
-        #print('loading ',os.path.join(self.samples_list[index]))
-        data, width, height =  read_dataset(self.samples_list[index])
+        # print('loading ',os.path.join(self.samples_list[index]))
+        data, width, height = read_dataset(self.samples_list[index])
         # transform data.ts into frames
-        data.ts = np.round(data.ts / np.max(data.ts) * (self.nframes-1))
+        data.ts = np.round(data.ts / np.max(data.ts) * (self.nframes - 1))
         # add relative index of the sequence
         data.ts = data.ts + index * self.nframes
         return np.array(data.ts, dtype=np.int64), np.array(data.x, dtype=np.int16), \
-            np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
-                self.target_list[index], self.digit_list[index]
+               np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
+               self.target_list[index], self.digit_list[index]
+
 
 class seqPatternEvents(Dataset):
-    def __init__(self, root, nframes=7, wevents=100, patt=[1,2,3], qsmp=3000, qrep=250):
+    def __init__(self, root, nframes=7, wevents=100, patt=[1, 2, 3], qsmp=3000, qrep=250):
         # generate a random list of all the files in nmnist with qsmp
         self.nframes = nframes
         self.wevents = wevents
@@ -161,48 +169,49 @@ class seqPatternEvents(Dataset):
         nros.sort()
         for nro in nros:
             # get file list
-            files = os.listdir(os.path.join(root,nro))
-            assert(len(files) > qsmp)
+            files = os.listdir(os.path.join(root, nro))
+            assert (len(files) > qsmp)
             shuffle(files)
             for i in range(qsmp):
-                samples_list_tmp.append(os.path.join(root,nro,files[i]))
+                samples_list_tmp.append(os.path.join(root, nro, files[i]))
                 target_list_tmp.append(0)
                 digit_list_tmp.append(nro)
             if int(nro) in self.patt:
-                #print('%s in patt' % nro)
+                # print('%s in patt' % nro)
                 shuffle(files)
                 cumpatt = []
                 for i in range(qrep):
-                    cumpatt.append(os.path.join(root,nro,files[i]))
-                self.pattern_list.append((cumpatt,nro))
+                    cumpatt.append(os.path.join(root, nro, files[i]))
+                self.pattern_list.append((cumpatt, nro))
         # now I need to shuffle the lists together
         c = list(zip(samples_list_tmp, target_list_tmp, digit_list_tmp))
         shuffle(c)
         samples_list_tmp, target_list_tmp, digit_list_tmp = zip(*c)
-        self.samples_list, self.target_list, self.digit_list = list(samples_list_tmp), list(target_list_tmp), list(digit_list_tmp)
-        #print(self.samples_list[0])
-        #print('length of pattern lists ', len(self.pattern_list))
+        self.samples_list, self.target_list, self.digit_list = list(samples_list_tmp), list(target_list_tmp), list(
+            digit_list_tmp)
+        # print(self.samples_list[0])
+        # print('length of pattern lists ', len(self.pattern_list))
         # now, insert qrep times the pattern in samples list
         nsmp_tmp = len(self.samples_list)
         gap = int(nsmp_tmp / (qrep))
-        #print('gap', gap)
+        # print('gap', gap)
         dx = int(0.1 * gap)
-        #print('dx', dx)
-        for k in range(1,qrep+1):
-            kind = k*gap + randint(-dx,dx)
-            #print('kind', kind)
+        # print('dx', dx)
+        for k in range(1, qrep + 1):
+            kind = k * gap + randint(-dx, dx)
+            # print('kind', kind)
             for i in range(len(self.pattern_list)):
-                #print(self.pattern_list[i][0])
-                self.samples_list.insert(kind+i,self.pattern_list[i][0][k-1])
-                self.target_list.insert(kind+i,1)
-                self.digit_list.insert(kind+i, self.pattern_list[i][1])
-                
+                # print(self.pattern_list[i][0])
+                self.samples_list.insert(kind + i, self.pattern_list[i][0][k - 1])
+                self.target_list.insert(kind + i, 1)
+                self.digit_list.insert(kind + i, self.pattern_list[i][1])
+
     def __len__(self):
         return len(self.samples_list)
-    
+
     def __getitem__(self, index):
-        #print('loading ',os.path.join(self.samples_list[index]))
-        data, width, height =  read_dataset(self.samples_list[index])
+        # print('loading ',os.path.join(self.samples_list[index]))
+        data, width, height = read_dataset(self.samples_list[index])
 
         # transform data into windows of Wevents:
         # Sort events by ts
@@ -212,21 +221,21 @@ class seqPatternEvents(Dataset):
         data[0].ts = 0
         for i in range(1, data.shape[0]):
             if i % self.wevents == 0:
-                time+=1
+                time += 1
             data[i].ts = time
 
         # Drop last events
         data = data[:self.wevents * self.nframes]
 
-
         # add relative index of the sequence
         data.ts = data.ts + index * self.nframes
         return np.array(data.ts, dtype=np.int64), np.array(data.x, dtype=np.int16), \
-            np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
-                self.target_list[index], self.digit_list[index]
+               np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
+               self.target_list[index], self.digit_list[index]
+
 
 class fixedPattern(Dataset):
-    def __init__(self, root, nframes=7, wevents=100, patt=[1,2,3], nDigits=3000, pfreq=10):
+    def __init__(self, root, nframes=7, wevents=100, patt=None, nDigits=3000, pfreq=10, noiseNums=None):
         """ 
             root    = dataset dir
             nframes = Number of frames per digit
@@ -236,6 +245,10 @@ class fixedPattern(Dataset):
             pfreq   = Every 'pfreq' digits, I insert the pattern
         """
         # generate a random list of all the files in nmnist with qsmp
+        if noiseNums is None:
+            noiseNums = []
+        if patt is None:
+            patt = [1, 2, 3]
         self.nframes = nframes
         self.wevents = wevents
         self.samples_list = []
@@ -248,35 +261,39 @@ class fixedPattern(Dataset):
         # Collect numbers for fixed pattern
         for nro in self.patt:
             # get file list
-            files = os.listdir(os.path.join(root,str(nro)))
+            files = os.listdir(os.path.join(root, str(nro)))
             # Choose random sample from number
             sample = random.choice(files)
-            self.pattern_list.append(os.path.join(root,str(nro),sample))
+            self.pattern_list.append(os.path.join(root, str(nro), sample))
 
         for i in range(1, nDigits):
+            # Check if I should insert the pattern
             if i % pfreq == 0:
+                # Insert pattern
                 for sample in range(len(self.pattern_list)):
                     self.samples_list.append(self.pattern_list[sample])
                     self.target_list.append(1)
                     self.digit_list.append(patt[sample])
             else:
                 # Choose a random number
-                nro = random.choice(nros)
+                if len(noiseNums) != 0:
+                    nro = random.choice(noiseNums)
+                else:
+                    nro = random.choice(nros)
                 # get file list
-                files = os.listdir(os.path.join(root,str(nro)))
+                files = os.listdir(os.path.join(root, str(nro)))
                 # Choose random sample from number
                 sample = random.choice(files)
-                self.samples_list.append(os.path.join(root,str(nro),sample))
+                self.samples_list.append(os.path.join(root, str(nro), sample))
                 self.target_list.append(0)
                 self.digit_list.append(nro)
 
-                
     def __len__(self):
         return len(self.samples_list)
-    
+
     def __getitem__(self, index):
-        #print('loading ',os.path.join(self.samples_list[index]))
-        data, width, height =  read_dataset(self.samples_list[index])
+        # print('loading ',os.path.join(self.samples_list[index]))
+        data, width, height = read_dataset(self.samples_list[index])
 
         # transform data into windows of Wevents:
         # Sort events by ts
@@ -286,19 +303,14 @@ class fixedPattern(Dataset):
         data[0].ts = 0
         for i in range(1, data.shape[0]):
             if i % self.wevents == 0:
-                time+=1
+                time += 1
             data[i].ts = time
 
         # Drop last events
         data = data[:self.wevents * self.nframes]
 
-
         # add relative index of the sequence
         data.ts = data.ts + index * self.nframes
         return np.array(data.ts, dtype=np.int64), np.array(data.x, dtype=np.int16), \
-            np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
-                self.target_list[index], self.digit_list[index]
-
-
-
-
+               np.array(data.y, dtype=np.int16), np.array(data.p, dtype=np.uint8), \
+               self.target_list[index], self.digit_list[index]
--- patterns/wevents_seqgen.py
+++ patterns/wevents_seqgen.py
@@ -9,7 +9,24 @@ import pan_functions
 from torch.utils.data import DataLoader
 import torch
 import numpy as np
+import argparse
 from os.path import dirname, realpath
+
+# parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-p", "--p", nargs='+', dest="patt_list", default=[1], action='store', help="Pattern list", type=int)
+parser.add_argument("-nn", "--nn", nargs='+', dest="noiseNums", default=None, action='store', help="Noisy Numbers", type=int)
+parser.add_argument("-w", "--w", dest="nevents", default=120, action='store', help="Number of events", type=int)
+parser.add_argument("-fname", "--fname", dest="fname", default="default_seq", action='store',
+                    help="File name of the sequence", type=str)
+
+args = parser.parse_args()
+
+patt_list = args.patt_list
+noiseNums = args.noiseNums
+nevents = args.nevents
+fname = args.fname
 
 root_dataset = './N-MNIST/Train'
 path = dirname(realpath(__file__))
@@ -19,23 +36,23 @@ path_seq_pat = path + '/sequences/'
 pan_functions.seed_torch(seed=0)
 
 # parameters of the dataset
-nframes = 15    # frames per digit
-wevents = 120   # number of events to take per ts
-patt = [1,2,3]  # pattern to copy throught the sequence
+nframes = 15  # frames per digit
+wevents = nevents  # number of events to take per ts
+patt = patt_list  # pattern to copy throught the sequence
 nDigits = 1000  # Amount of digits to include in the random sequence
-pfreq = 5      # Every 'pfreq' digits, I insert the pattern
-qsmp=27         # number of samples of each digit to include in the sequence
-qrep=30         # number of times the patter is repeated on the sequence
+pfreq = 3  # Every 'pfreq' digits, I insert the pattern
+qsmp = 27  # number of samples of each digit to include in the sequence
+qrep = 30  # number of times the patter is repeated on the sequence
 
 # Pablo's function
-#train_seq = pan_functions.seqPatternEvents(root=root_dataset,nframes=nframes, 
+# train_seq = pan_functions.seqPatternEvents(root=root_dataset,nframes=nframes,
 #                      wevents=wevents, patt=patt, qsmp=qsmp, qrep=qrep)
-#time = (nframes*len(patt)*qrep) + (10*qsmp*nframes)
-#print(str(time))
+# time = (nframes*len(patt)*qrep) + (10*qsmp*nframes)
+# print(str(time))
 
 # Maxi's function
-train_seq = pan_functions.fixedPattern(root=root_dataset, nframes=nframes, 
-                      wevents=wevents, patt=patt, nDigits=nDigits, pfreq=pfreq)
+train_seq = pan_functions.fixedPattern(root=root_dataset, nframes=nframes,
+                                       wevents=wevents, patt=patt, nDigits=nDigits, pfreq=pfreq, noiseNums=noiseNums)
 
 # Calculate total time
 time = len(train_seq) * nframes
@@ -46,31 +63,31 @@ print('Dataset reading resulting on %d samples' % len(train_seq))
 # to conserve the structure of the sequence
 train_loader = DataLoader(train_seq, batch_size=1, shuffle=False)
 
-spike_times = np.zeros([34, 34, time]) # Create matrix of spike times
-pat_times = np.zeros(time) # Indicator of the pattern in the sequence
+spike_times = np.zeros([34, 34, time])  # Create matrix of spike times
+pat_times = np.zeros(time)  # Indicator of the pattern in the sequence
 # Save complete sequence
 data = []
 
-for ts,x,y,p, target, digit in train_loader:
+for ts, x, y, p, target, digit in train_loader:
     x = torch.squeeze(x)
     y = torch.squeeze(y)
     p = torch.squeeze(p)
     ts = torch.squeeze(ts)
-    #print("Max index: " + str(y.max()) + str(x.max()) + str(ts.max()))
+    # print("Max index: " + str(y.max()) + str(x.max()) + str(ts.max()))
     for i in range(p.shape[0]):  # iterate over the polarity array
-        #if p[i]:  # if polarity is positive
+        # if p[i]:  # if polarity is positive
         index = (y[i], x[i], ts[i])
         spike_times[index] = 1
         if target[0]:
             pat_times[ts[i]] = 1
         # get info to show sequence:
-        data.append( (x[i].item(), y[i].item(), p[i].item(), ts[i].item()) )
-    #print('%s -> sq [ %d, %d ] -> size %d -> t: %d' % (digit[0], ts.min(), ts.max(), 
+        data.append((x[i].item(), y[i].item(), p[i].item(), ts[i].item()))
+    # print('%s -> sq [ %d, %d ] -> size %d -> t: %d' % (digit[0], ts.min(), ts.max(),
     #                                                   ts.shape[0], target[0]))
 
-#print("Sequence length: " + str(time))
+# print("Sequence length: " + str(time))
 data = np.array(data, np.dtype('uint16, uint16, uint8, uint64'))
 
-np.save(path_seq_pat + 'seq_0', spike_times)
-np.save(path_seq_pat + 'pat_i_0', pat_times)
-np.save(path_seq_pat + 'data_0', data)
+np.save(path_seq_pat + fname, spike_times)
+np.save(path_seq_pat + 'pat_' + fname, pat_times)
+np.save(path_seq_pat + 'data_' + fname, data)
