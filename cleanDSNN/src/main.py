761ec03ce3a6a568cd85ab8b6713dda62938562a
Maxi Martino <mmartino@medallia.com>
2021-08-28 20:18:00 -0300

    Test convergence method

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -19,7 +19,7 @@ def main():
     learn_SDNN = True   # This flag toggles between Learning STDP and classify features
                         # or just classify by loading pretrained weights
     free_spikes = False  # This flag toggles whether we allowed neurons to spikes every timestep or once per timeframe
-    c_learning = False   # This flag toggles whether we learn by a fixed amount of iterationts or when weights converges
+    c_learning = True   # This flag toggles whether we learn by a fixed amount of iterationts or when weights converges
 
     if learn_SDNN:
         set_weights = False  # Loads the weights from a path (path_set_weigths) and prevents any SDNN learning
