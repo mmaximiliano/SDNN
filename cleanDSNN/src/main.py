63a886051c1c7657a48b7e845a5c4d77600cd61d
Maxi Martino <mmartino@medallia.com>
2021-08-30 19:31:50 -0300

    Reduce th

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -41,10 +41,10 @@ def main():
     # SDNN_cuda parameters
     frame_time = 15
     network_params = [{'Type': 'input', 'num_filters': 1, 'pad': (0, 0), 'H_layer': 34, 'W_layer': 34},
-                      {'Type': 'conv', 'num_filters': 8, 'filter_size': 7, 'th': 15.,
+                      {'Type': 'conv', 'num_filters': 8, 'filter_size': 7, 'th': 10.,
                        'alpha': .99, 'beta': 0., 'delay': 0},
                       {'Type': 'pool', 'num_filters': 8, 'filter_size': 2, 'th': 0., 'stride': 2},
-                      {'Type': 'conv', 'num_filters': 16, 'filter_size': 5, 'th': 10.,
+                      {'Type': 'conv', 'num_filters': 16, 'filter_size': 5, 'th': 6.,
                        'alpha': .99, 'beta': 0., 'delay': 0},
                       {'Type': 'pool', 'num_filters': 16, 'filter_size': 3, 'th': 0., 'stride': 2},
                       {'Type': 'conv', 'num_filters': 16, 'filter_size': 3, 'th': 2.,
