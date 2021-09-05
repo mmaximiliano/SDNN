033b3a6d5ec54fa5d7ed0d873676b15ce10cb92a
Maxi Martino <mmartino@medallia.com>
2021-09-05 17:42:02 -0300

    Update main.py

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -41,13 +41,13 @@ def main():
     # SDNN_cuda parameters
     frame_time = 15
     network_params = [{'Type': 'input', 'num_filters': 1, 'pad': (0, 0), 'H_layer': 34, 'W_layer': 34},
-                      {'Type': 'conv', 'num_filters': 8, 'filter_size': 7, 'th': 12.,
+                      {'Type': 'conv', 'num_filters': 12, 'filter_size': 7, 'th': 11.,
                        'alpha': .99, 'beta': 0., 'delay': 0},
-                      {'Type': 'pool', 'num_filters': 8, 'filter_size': 2, 'th': 0., 'stride': 2},
-                      {'Type': 'conv', 'num_filters': 20, 'filter_size': 5, 'th': 8.,
+                      {'Type': 'pool', 'num_filters': 12, 'filter_size': 2, 'th': 0., 'stride': 2},
+                      {'Type': 'conv', 'num_filters': 20, 'filter_size': 5, 'th': 7.5,
                        'alpha': .99, 'beta': 0., 'delay': 0},
                       {'Type': 'pool', 'num_filters': 20, 'filter_size': 3, 'th': 0., 'stride': 2},
-                      {'Type': 'conv', 'num_filters': 20, 'filter_size': 3, 'th': 1.7,
+                      {'Type': 'conv', 'num_filters': 20, 'filter_size': 3, 'th': 1.5,
                        'alpha': .99, 'beta': 0., 'delay': 0}]
     weight_params = {'mean': 0.8, 'std': 0.01}
 
