4cc34740c0c9a8cef5b5ef384e43653896e646d1
Maxi Martino <mmartino@medallia.com>
2021-09-07 13:31:16 -0300

    Prepare for 'all_nums_5'

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -51,7 +51,7 @@ def main():
                        'alpha': .99, 'beta': 0., 'delay': 0}]
     weight_params = {'mean': 0.8, 'std': 0.01}
 
-    max_learn_iter = [0, 3500, 0, 5000, 0, 7500, 0]
+    max_learn_iter = [0, 3000, 0, 4000, 0, 6000, 0]
     stdp_params = {'max_learn_iter': max_learn_iter,
                    'stdp_per_layer': [0, 10, 0, 4, 0, 2],
                    'max_iter': sum(max_learn_iter),
