a553856c4ca86f3251a513784049ad7d4c8d5c3f
Maxi Martino <mmartino@medallia.com>
2021-09-23 19:03:15 -0300

    prepare for massive runs

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -11,6 +11,7 @@ import numpy as np
 from math import floor
 import argparse
 import pathlib
+import singleNeuron as sn
 
 import time
 
@@ -46,12 +47,17 @@ def main():
     path_seq_test = spike_times_pat_seq + "testing/" + seed + "test_" + file_name
     path_seq_dif_sample = '../../patterns/sequences/' + "all_nums_dif_sample.npy"
 
+    # Sequences pattern directories
+    path_pat_test = spike_times_pat_seq + "testing/" + seed + "path_test_" + file_name
+    path_pat_dif_sample = '../../patterns/sequences/' + "pat_all_nums_dif_sample.npy"
+
     # Results directories
     path_set_weigths = '../results/1/' + file_name + '/weights/' + seed + '/'
     path_save_weigths = '../results/1/' + file_name + '/weights/' + seed + '/'
     path_spikes_out_training = '../results/1/' + file_name + '/training/' + seed + '/'
     path_spikes_out_testing = '../results/1/' + file_name + '/testing/' + seed + '/'
     path_spikes_out_dif_sample = '../results/1/' + file_name + 'dif_sample/' + seed + '/'
+    path_save_metrics = '../results/1/' + file_name + 'metrics/' + seed + '/'
 
     # ------------------------------- SDNN -------------------------------#
     # SDNN_cuda parameters
@@ -103,8 +109,10 @@ def main():
                 np.save(path_save_weigths + 'weight_'+str(i), weights[i])
 
     # ------------------------------- Run Testing Sequence & Save Results -------------------------------#
+    print("Starting testing without training.")
 
     # Running Training sequence
+    print("Test train sequence")
     Sin_tmp_train = first_net.train_features(path_seq_train)
     fname = 'layer_' + str(5) + '_' + str(network_params[5]['Type'])
     Sin_train = torch.tensor(Sin_tmp_train[5])
@@ -112,6 +120,7 @@ def main():
     torch.save(Sin_train, path_spikes_out_training + fname + '.pt')
 
     # Running Testing sequence
+    print("Test test sequence")
     Sin_tmp_test = first_net.train_features(path_seq_test)
     fname = 'layer_' + str(5) + '_' + str(network_params[5]['Type'])
     Sin_test = torch.tensor(Sin_tmp_test[5])
@@ -119,43 +128,130 @@ def main():
     torch.save(Sin_test, path_spikes_out_testing + fname + '.pt')
 
     # Running Dif Sample sequence
+    print("Test dif_sample sequence")
     Sin_tmp_dif_sample = first_net.train_features(path_seq_dif_sample)
     fname = 'layer_' + str(5) + '_' + str(network_params[5]['Type'])
     Sin_dif_sample = torch.tensor(Sin_tmp_dif_sample[5])
     pathlib.Path(path_spikes_out_dif_sample).mkdir(parents=True, exist_ok=True)
     torch.save(Sin_dif_sample, path_spikes_out_dif_sample + fname + '.pt')
 
-    print('DONE')
-
     # -------------------------- Single Neuron Output -------------------------- #
-    #Sin_tmp = torch.tensor(Sin_tmp[len(network_params)-1])
-    #T = Sin_tmp.shape[0]
-    #N_out = 1
-    #N_in = Sin_tmp.shape[1]
-    #th = 3.8
+    print("Start Single Neuron Training and Testing for each th value")
+    for th in [1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 99.0, 98.0]:
+        if th == 99.0:
+            str_th = "2_5-3_0"
+        elif th == 98.0:
+            str_th = "2_5-3_5"
+        else:
+            str_th = str(th)
+            str_th = str_th[0] + '_' + str_th[2]
 
-    #singleNeuron = STDPLIFDensePopulation(in_channels=N_in, out_channels=N_out,
-    #                                      weight=0.75, alpha=float(np.exp(-1e-3 / 10e-3)),
-    #                                      beta=float(np.exp(-1e-3 / 2e-5)), delay=0,
-    #                                      th=th, a_plus=.03225, a_minus=.045625,
-    #                                      w_max=1.)
-    #Sin = Sin_tmp.clone().detach()
+        more_th = False
+        new_th = th
+        if th == 99.0:
+            th = 2.5
+            new_th = 3.0
+            more_th = True
+        elif th == 98.0:
+            th = 2.5
+            new_th = 3.5
+            more_th = True
 
-    # Pre-procesamos PSpikes y NSpikes
-    #dt_ltp = 10  # Cantidad de timesteps que miro hacia atras
-    #dt_ltd = 50  # Cantidad de timesteps que miro hacia delante
-    #PSpikes = preSpikes(T, dt_ltp, torch.zeros(T, N_in), Sin)
-    #NSpikes = nextSpikes(T, dt_ltd, torch.zeros(T, N_in), Sin)
+        # ===== Train Single Neuron ===== #
+        Sin = Sin_train.clone().detach()
+        T = Sin.shape[0]
+        N_out = 1
+        N_in = Sin.shape[1]
 
-    # Realizamos el entrenamiento STDP
-    #Uprobe = np.empty([T, N_out])
-    #Iprobe = np.empty([T, N_out])
-    #Sprobe = np.empty([T, N_out])
-    #for n in range(T):
-    #    state = singleNeuron.forward(Sin[n].unsqueeze(0), PSpikes[n], NSpikes[n - 1])
-    #    Uprobe[n] = state.U.data.numpy()
-    #    Iprobe[n] = state.I.data.numpy()
-    #    Sprobe[n] = state.S.data.numpy()
+        singleNeuron = sn.STDPLIFDensePopulation(in_channels=N_in, out_channels=N_out,
+                                                weight=0.75, alpha=float(np.exp(-1e-3/10e-3)),
+                                                beta=float(np.exp(-1e-3/2e-5)), delay=0,
+                                                th=th, a_plus=.03125, a_minus=.04125,
+                                                w_max=1.)
+
+        # Pre-procesamos PSpikes y NSpikes
+        dt_ltp = 20  # Cantidad de timesteps que miro hacia atras
+        dt_ltd = 70  # Cantidad de timesteps que miro hacia delante
+        PSpikes = sn.preSpikes(T, dt_ltp, torch.zeros(T, N_in), Sin)
+        NSpikes = sn.nextSpikes(T, dt_ltd, torch.zeros(T, N_in), Sin)
+
+        # Realizamos el entrenamiento STDP
+        Uprobe = np.empty([T, N_out])
+        Iprobe = np.empty([T, N_out])
+        Sprobe = np.empty([T, N_out])
+
+        n = 0
+        t = 0
+        c_l = 1.
+        while (c_l > 0.01) and (t < 550000):
+            state = singleNeuron.forward(Sin[n].unsqueeze(0), PSpikes[n], NSpikes[n-1])
+            Uprobe[n] = state.U.data.numpy()
+            Iprobe[n] = state.I.data.numpy()
+            Sprobe[n] = state.S.data.numpy()
+            n += 1
+            if n >= T:
+                n = 0
+
+            if more_th and (t == 400000):
+                singleNeuron.th = new_th
+            c_l = sn.convergence_level(singleNeuron.fc_layer.weight[0])
+            t += 1
+
+        # ===================== Test Single Neuron ===================== #
+        pat_times = np.load(path_pat_test)
+        Sin = Sin_test
+        T = Sin.shape[0]
+        N_out = 1
+
+        if more_th:
+            singleNeuron.th = 3.75
+
+        # Realizamos el testing
+        Uprobe = np.empty([T, N_out])
+        Iprobe = np.empty([T, N_out])
+        Sprobe = np.empty([T, N_out])
+        for n in range(T):
+            state = singleNeuron.forward_no_learning(Sin[n].unsqueeze(0))
+            Uprobe[n] = state.U.data.numpy()
+            Iprobe[n] = state.I.data.numpy()
+            Sprobe[n] = state.S.data.numpy()
+
+        # Corremos metricas
+        # remove last part of sequence since its wrong
+        accuracy, precision, recall, f1, fake_alarms, missed_alarms = sn.get_metrics(T - 15, pat_times, Sprobe)
+
+        metrics = np.array([accuracy, precision, recall, f1, fake_alarms, missed_alarms], dtype=float)
+        pathlib.Path(path_save_metrics).mkdir(parents=True, exist_ok=True)
+        np.save(path_save_metrics + 'test_metrics_' + str_th + '.npy', metrics)
+
+        # ===================== Dif Sample Single Neuron ===================== #
+        pat_times = np.load(path_pat_dif_sample)
+        Sin = Sin_dif_sample
+        T = Sin.shape[0]
+        N_out = 1
+
+        if more_th:
+            singleNeuron.th = 3.75
+
+        # Realizamos el testing
+        Uprobe = np.empty([T, N_out])
+        Iprobe = np.empty([T, N_out])
+        Sprobe = np.empty([T, N_out])
+        for n in range(T):
+            state = singleNeuron.forward_no_learning(Sin[n].unsqueeze(0))
+            Uprobe[n] = state.U.data.numpy()
+            Iprobe[n] = state.I.data.numpy()
+            Sprobe[n] = state.S.data.numpy()
+
+        # Corremos metricas
+        # remove last part of sequence since its wrong
+        accuracy, precision, recall, f1, fake_alarms, missed_alarms = sn.get_metrics(T - 15, pat_times, Sprobe)
+
+        metrics = np.array([accuracy, precision, recall, f1, fake_alarms, missed_alarms], dtype=float)
+        pathlib.Path(path_save_metrics).mkdir(parents=True, exist_ok=True)
+        np.save(path_save_metrics + 'dif_sample_metrics_' + str_th + '.npy', metrics)
+
+    print('Done.')
 
 if __name__ == '__main__':
     start = time.time()
