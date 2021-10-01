f5860849c20016324f8b89603e03176d4a2b1dbb
Maxi Martino <mmartino@medallia.com>
2021-10-01 11:35:14 -0300

    Update main.py

--- cleanDSNN/src/main.py
+++ cleanDSNN/src/main.py
@@ -63,10 +63,10 @@ def main():
     frame_time = 15
     network_params = [{'Type': 'input', 'num_filters': 1, 'pad': (0, 0), 'H_layer': 34, 'W_layer': 34},
                       {'Type': 'conv', 'num_filters': 8, 'filter_size': 7, 'th': 15.,
-                       'alpha': .99, 'beta': 0., 'delay': 30},
+                       'alpha': .99, 'beta': 0., 'delay': 15},
                       {'Type': 'pool', 'num_filters': 32, 'filter_size': 2, 'th': 0., 'stride': 2},
                       {'Type': 'conv', 'num_filters': 20, 'filter_size': 5, 'th': 10.,
-                       'alpha': .99, 'beta': 0., 'delay': 15},
+                       'alpha': .99, 'beta': 0., 'delay': 0},
                       {'Type': 'pool', 'num_filters': 20, 'filter_size': 3, 'th': 0., 'stride': 2},
                       {'Type': 'conv', 'num_filters': 20, 'filter_size': 3, 'th': 2.,
                        'alpha': .99, 'beta': 0., 'delay': 0}]
