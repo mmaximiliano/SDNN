2adea8bda98feece4c8cc29fef45e91a9fb564cb
Maxi Martino <mmartino@medallia.com>
2021-10-16 18:44:11 -0300

    Update snn_runner.sh

--- cleanDSNN/src/snn_runner.sh
+++ cleanDSNN/src/snn_runner.sh
@@ -6,9 +6,9 @@ c=1
 
 for fname in "all_nums"
 do
-	for seed in 0 1 2 3 4 5 6 7 8 9
+	for seed in 0 1 2 3 4
   do
-  	echo "Loop: $c/10"
+  	echo "Loop: $c/5"
   	echo "Running fname= $fname seed= $seed"
   	python3 main.py -fname $fname -s $seed
   	echo
