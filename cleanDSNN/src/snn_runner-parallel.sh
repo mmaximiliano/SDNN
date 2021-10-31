b52c9b9773e79d77bb7d2a4dfbfeae3bb8667f0b
Maxi Martino <mmartino@medallia.com>
2021-10-31 20:31:08 -0300

    Prepare for parallel work

---
+++ cleanDSNN/src/snn_runner-parallel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "----- Running massive sequence creator -----"
+
+c=1
+
+for fname in "all_nums_5" "all_nums_20"
+do
+	for seed in 0 1 2 3 4 5 6 7 8 9
+  do
+  	echo "Loop: $c/20"
+  	echo "Running fname= $fname seed= $seed"
+  	python3 main-parallel.py -fname $fname -s $seed
+  	echo
+  	c=$((c+1))
+	done
+done
+
+echo "Done."
