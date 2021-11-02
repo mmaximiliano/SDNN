78d9fb4b11810f7e38c5a0fbc47ece25b82abeea
Maxi Martino <mmartino@medallia.com>
2021-11-01 22:02:22 -0300

    final fix

--- cleanDSNN/src/snn_runner-delay.sh
+++ cleanDSNN/src/snn_runner-delay.sh
@@ -12,7 +12,7 @@ do
     do
   	  echo "Loop: $c/343"
   	  echo "Running c_1= $c_1 c_2= $c_2 c_3= $c_3"
-  	  python3 main.py -c1 $c_1 -c2 $c_2 -c3 $c_3
+  	  python3 main-delay.py -c1 $c_1 -c2 $c_2 -c3 $c_3
   	  echo
   	  c=$((c+1))
   	done
