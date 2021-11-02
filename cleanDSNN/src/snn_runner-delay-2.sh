600be0ee097cee3106bebba88ec22d18524ccacf
Maxi Martino <mmartino@medallia.com>
2021-11-02 17:25:21 -0300

    Add more files to run in parallel

---
+++ cleanDSNN/src/snn_runner-delay-2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "----- Running massive sequence creator -----"
+
+c=1
+
+for c_1 in 25
+do
+	for c_2 in 0 5 10 15 20 25 30
+  do
+    for c_3 in 0 5 10 15 20 25 30
+    do
+  	  echo "Loop: $c/49"
+  	  echo "Running c_1= $c_1 c_2= $c_2 c_3= $c_3"
+  	  python3 main-delay.py -c1 $c_1 -c2 $c_2 -c3 $c_3
+  	  echo
+  	  c=$((c+1))
+  	done
+	done
+done
+
+echo "Done."
