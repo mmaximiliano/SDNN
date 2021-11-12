2d6779fcd734ed5956754c24cf0b14bd3bbd1160
Maxi Martino <mmartino@medallia.com>
2021-11-11 21:56:36 -0300

    Update snn_runner.sh

--- cleanDSNN/src/snn_runner.sh
+++ cleanDSNN/src/snn_runner.sh
@@ -4,11 +4,11 @@ echo "----- Running massive sequence creator -----"
 
 c=1
 
-for fname in "all_nums"
+for fname in "all_nums_20"
 do
-	for seed in 0 7 9
+	for seed in 9
   do
-  	echo "Loop: $c/30"
+  	echo "Loop: $c/1"
   	echo "Running fname= $fname seed= $seed"
   	python3 main.py -fname $fname -s $seed
   	echo
